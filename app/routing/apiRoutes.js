2898b6cefce04711b0b7ccc8aab166a20c7896be
Zachary Pleasant <zpleasant10@gmail.com>
2018-03-01 18:19:44 -0600

    Set up html/api routing. Created array to hold JSON data.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        friendsData.push(req.body);
+        
+        console.log(friendsData);
+    });
+};
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
