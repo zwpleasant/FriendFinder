f2ef0baca545ab53ddd6beb28ad4fd6b241fc774
Zachary Pleasant <zpleasant10@gmail.com>
2018-03-04 17:50:57 -0600

    For loop through newFriend object is not working.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,9 +4,28 @@ module.exports = function(app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
-
+    
     app.post("/api/friends", function (req, res) {
-        friendsData.push(req.body);
-        res.json(true);
+        var newFriend = req.body
+        console.log(req.body)
+        // run through loops to parse JSON friend array and check scores with absolute values
+        // req.body is coming back in console, but is showing undefined in for loop..
+        var findLowScore = [];
+        for (var i = 0; i < friendsData.length; i++) {
+            var differences = []
+            for (var j = 0; j < friendsData[i].scores.length; j++) {
+                differences.push(Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriend.scores[j])));
+            }
+            findLowScore.push(differences.reduce((accumulator, currentValue) => accumulator + currentValue));
+        }
+        friendsData.push(newFriend);
+
+        var k = findLowest.indexOf(Math.min.apply(null, findLowest))
+        var match = {
+            name: friendsData[k].name,
+            photo: friendsData[k].photo
+        }
+        res.send(`<h1>You matched: <p>${match.name}</p></h1><br>
+        <img src=${match.photo}>`)
     });
 };
