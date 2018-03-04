c99101ed513597068dd5820adae033e8c6019a6f
Zachary Pleasant <zpleasant10@gmail.com>
2018-03-04 16:15:35 -0600

    Added three friends for friends array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendArray = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"LeBron",
+        "photo":"https://b.fssta.com/uploads/application/nba/players/214152.vresize.350.425.medium.35.png",
         "scores":[
             5,
             1,
@@ -14,7 +14,39 @@ var friendArray = [
             4,
             1
           ]
-      }
+    },
+    {
+        "name":"2Chainz",
+        "photo":"http://instantrapairhorn.com/wp-content/uploads/2014/03/btn16.png",
+        "scores":[
+            1,
+            3,
+            4,
+            2,
+            5,
+            3,
+            5,
+            4,
+            5,
+            5
+          ]
+    },
+    {
+        "name":"Elon",
+        "photo":"http://www.stickpng.com/assets/images/580b57fbd9996e24bc43be25.png",
+        "scores":[
+            1,
+            5,
+            2,
+            2,
+            1,
+            3,
+            3,
+            2,
+            1,
+            2
+          ]
+    }
 ];
 
 module.exports = friendArray;
