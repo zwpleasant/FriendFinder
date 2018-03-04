1493b084505fad4c2e8995e8028ccba3c4f18e84
Zachary Pleasant <zpleasant10@gmail.com>
2018-03-04 16:03:05 -0600

    server.js: changed extended to true for arrays. survey.html: added in modal and started POST.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes")(app);
